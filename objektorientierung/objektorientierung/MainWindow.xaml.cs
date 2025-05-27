f8fa956f72751b2ae55ced6e056e68f3a7ce70ec
florian.wagner <florian.wagner@htlwy.at>
2025-05-27 16:07:35 +0200

    x und y verschiebung inklusive zeichnen

--- objektorientierung/objektorientierung/MainWindow.xaml.cs
+++ objektorientierung/objektorientierung/MainWindow.xaml.cs
@@ -19,15 +19,19 @@ namespace objektorientierung
     {
         public double laenge = 1;
         public double breite = 2;
+        public double x = 0;
+        public double y = 0;
 
         public double FlaechenBerechnen() 
         {
             return laenge * breite;
         }
-         public Rechteck(double laenge, double breite) 
+         public Rechteck(double laenge, double breite, double x, double y) 
         {
             this.laenge = laenge;
             this.breite = breite;
+            this.y= y;
+            this.x = x;
             
         }
         public override string ToString()
@@ -61,17 +65,23 @@ namespace objektorientierung
                 double laenge = double.Parse(laengeStr);
                 string breiteStr = this.tbxbreite.Text;
                 double breite = double.Parse(breiteStr);
-                if(lstrechtecke.SelectedItem != null)
+                string xStr = this.tbxx.Text;
+                double x = double.Parse(laengeStr);
+                string yStr = this.tbxy.Text;
+                double y = double.Parse(breiteStr);
+                if (lstrechtecke.SelectedItem != null)
                 {
                     Rechteck r=(Rechteck)lstrechtecke.SelectedItem;
                     r.breite= breite;
                     r.laenge= laenge;
+                    r.x = x;
+                    r.y = y;
                     lstrechtecke.Items.Refresh();
 
                 }
                 else
                 {
-                    Rechteck r = new Rechteck(laenge, breite);
+                    Rechteck r = new Rechteck(laenge, breite, x, y);
                     lstrechtecke.Items.Add(r);
                     rechtecke.Add(r);
                     lstrechtecke.Items.Refresh();
@@ -103,15 +113,23 @@ namespace objektorientierung
 
         private void btnrechteckezeichnen(object sender, RoutedEventArgs e)
         {
-         
+
+            for (int i=0; i<rechtecke.Count; i++)
+            {
+
+                Rectangle rectangle = new Rectangle();
+                double x = rechtecke[i].x;
+                double y = rechtecke[i].y;
+                rectangle.StrokeThickness = 2;
+                rectangle.Stroke = Brushes.Red;
+                rectangle.Width = rechtecke[i].breite;
+                rectangle.Height = rechtecke[i].laenge;
+                rectangle.Margin = new Thickness(5);
+                myCanvas.Children.Add(rectangle);
+                Canvas.SetLeft(rectangle, y);
+                Canvas.SetTop(rectangle, x);
+            };
             
-            Rectangle rectangle = new Rectangle();
-            rectangle.StrokeThickness = 2;
-            rectangle.Stroke = Brushes.Red;
-            rectangle.Width = breite;
-            rectangle.Height = laenge;
-            rectangle.Margin = new Thickness(5);
-            myCanvas.Children.Add(rectangle);
         }
     }
 }
